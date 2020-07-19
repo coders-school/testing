c6533c91878e07ea99b85254d759f259bb51bdd2
WojtekMs <wojtek.atmo@gmail.com>
2020-07-19 17:31:36 +0200

    changing point count from 10 to 20

--- bowling/tests.cpp
+++ bowling/tests.cpp
@@ -3,7 +3,7 @@
 SCENARIO("throw tests", "[throws]") {
     GIVEN("player makes a move / frame started") {
         WHEN("player already made 2 throws") {
-            THEN("point count for this frame is <= 10") {
+            THEN("point count for this frame is <= 20") {
             }
         }
     }
