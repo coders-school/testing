ae0b3f287d1cbf79d9f7f345882f80d783c27a91
WojtekMs <wojtek.atmo@gmail.com>
2020-07-19 17:35:14 +0200

    reformating tests.cpp according to chromium style

--- bowling/tests.cpp
+++ bowling/tests.cpp
@@ -1,10 +1,9 @@
 #include "catch.hpp"
 
 SCENARIO("throw tests", "[throws]") {
-    GIVEN("player makes a move / frame started") {
-        WHEN("player already made 2 throws") {
-            THEN("point count for this frame is <= 20") {
-            }
-        }
+  GIVEN("player makes a move / frame started") {
+    WHEN("player already made 2 throws") {
+      THEN("point count for this frame is <= 20") {}
     }
+  }
 }
