ba10efa03513213213245be2b90b1c704332e805
jakjan95 <jakubjanusz95@gmail.com>
2020-07-20 10:57:41 +0200

    test case for ten strike

--- bowling/bowling.ut.cpp
+++ bowling/bowling.ut.cpp
@@ -7,12 +7,24 @@ int score(int number) {
 }
 
 SCENARIO("Miss in every game") {
-    GIVEN("Ten turns of mage") {
+    GIVEN("Ten turns of game") {
         std::vector<int> game(20, 0);
         WHEN("Missing in every turn") {
             int gameScore = score(0);
             THEN("Score should be 0 points") {
                 REQUIRE(gameScore == 0);
+            }
+        }
+    }
+}
+
+SCENARIO("Ten strikes - perfect game") {
+    GIVEN("Ten turns of game") {
+        std::vector<int> game(10, 10);
+        WHEN("Getting strike in every turn") {
+            int gameScore = score(300);
+            THEN("Score should be 300 points") {
+                REQUIRE(gameScore == 300);
             }
         }
     }
