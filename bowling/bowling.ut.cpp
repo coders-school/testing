35fdca8b9f1c356ad5a771702345b8fe0d256557
jakjan95 <jakubjanusz95@gmail.com>
2020-07-20 11:01:41 +0200

    test case for strike in last turn

--- bowling/bowling.ut.cpp
+++ bowling/bowling.ut.cpp
@@ -6,7 +6,7 @@ int score(int number) {
     return number;
 }
 
-SCENARIO("Miss in every game") {
+SCENARIO("Miss in every game", "[points]") {
     GIVEN("Ten turns of game") {
         std::vector<int> game(20, 0);
         WHEN("Missing in every turn") {
@@ -18,13 +18,30 @@ SCENARIO("Miss in every game") {
     }
 }
 
-SCENARIO("Ten strikes - perfect game") {
+SCENARIO("Ten strikes - perfect game", "[points]") {
     GIVEN("Ten turns of game") {
         std::vector<int> game(10, 10);
         WHEN("Getting strike in every turn") {
             int gameScore = score(300);
             THEN("Score should be 300 points") {
                 REQUIRE(gameScore == 300);
+            }
+        }
+    }
+}
+
+SCENARIO("Strike in last turn") {
+    GIVEN("Ten turns of game") {
+        std::vector<int> game(9, 0);
+        game.push_back(10);
+        auto lengthBeforeRolling = game.size();
+        game.push_back(5);
+        game.push_back(5);
+
+        WHEN("Getting strike in every turn") {
+            int lengthAfterRolling = game.size();
+            THEN("Score should be 300 points") {
+                REQUIRE(lengthBeforeRolling + 2 == lengthAfterRolling);
             }
         }
     }
