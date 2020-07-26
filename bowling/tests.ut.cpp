2c89de18dc7f6487ece7a260798f9ebca7732082
Tomonike <pgurtowski1@gmail.com>
2020-07-26 19:01:10 +0200

    Add check if frame ends after spare

--- bowling/tests.ut.cpp
+++ bowling/tests.ut.cpp
@@ -1,18 +1,26 @@
 #include "catch.hpp"
 
 SCENARIO("frame tests", "[frames]") {
-  GIVEN("throws: 10, 3, 4, 10, 5, 5, 1") {
-    WHEN("actual frame is calculating") {
-      THEN("return frame number == 5") {
-      }
+    GIVEN("throws: 10, 3, 4, 10, 5, 5, 1") {
+        WHEN("actual frame is calculating") {
+            THEN("return frame number == 5") {
+            }
+        }
     }
-  }
 }
 
 SCENARIO("throw tests", "[throws]") {
-  GIVEN("player makes a move / frame started") {
-    WHEN("player already made 2 throws") {
-      THEN("point count for this frame is <= 20") {}
+    GIVEN("player makes a move / frame started") {
+        WHEN("player already made 2 throws") {
+            THEN("point count for this frame is <= 20") {}
+        }
     }
-  }
+}
+//test bool spareTrue
+bool spareTrue() {
+    return 0 == 0;
+}
+
+TEST_CASE("The frame end after spare", "[frames]") {
+    REQUIRE(spareTrue() == true);
 }
