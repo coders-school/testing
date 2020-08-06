ba165ab769a2e0532d5625ad5f6f5696ba4a13a9
WojtekMs <wojtek.atmo@gmail.com>
2020-08-06 22:30:26 +0200

    loadFromFile function added

--- bowling/Game.cpp
+++ bowling/Game.cpp
@@ -1,14 +1,56 @@
 #include "Game.hpp"
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 
 void Game::loadFromFile(const std::string& fileName) {
-    std::ifstream file(fileName);
+    std::fstream file(fileName, std::ios_base::in);
     if (!file.good()) {
         std::cerr << "file " << fileName << " could not be opened!\n";
         abort();
     }
-
+    std::string lineText{};
+    while (getline(file, lineText)) {
+        int currentCharIndex = 0;
+        PlayerData player;
+        fillPlayerName(currentCharIndex, player.playerName, lineText);
+        fillPlayerRolls(currentCharIndex, player.playerRolls, lineText);
+        players.push_back(player);
+    }
     file.close();
+}
+
+void Game::fillPlayerName(int& currentCharIndex, std::string& playerName, const std::string& lineText) {
+    for (currentCharIndex; currentCharIndex < lineText.size(); ++currentCharIndex) {
+        if (lineText[currentCharIndex] != ':') {
+            playerName += lineText[currentCharIndex];
+        } else {
+            currentCharIndex++;
+            break;
+        }
+    }
+
+}
+
+void Game::fillPlayerRolls(int& currentCharIndex, std::vector<Frame>& playerRolls, const std::string& lineText) {
+    for (currentCharIndex; currentCharIndex < lineText.size(); ++currentCharIndex) {
+        if (lineText[currentCharIndex] == 'X') {
+            playerRolls.push_back(Frame('X', ' '));
+            currentCharIndex++;
+        } else {
+            playerRolls.push_back(
+                Frame(lineText[currentCharIndex], lineText[currentCharIndex + 1]));
+            currentCharIndex += 2;
+        }
+        if (playerRolls.size() == 10) {
+            if (lineText[currentCharIndex - 1] == 'X') {
+                playerRolls.push_back(Frame(lineText[currentCharIndex + 2],
+                                            lineText[currentCharIndex + 3]));
+            } else if (lineText[currentCharIndex - 1] == '/') {
+                playerRolls.push_back(Frame(lineText[currentCharIndex + 2], ' '));
+            }
+            break;
+        }
+    }
 }
--- bowling/Game.hpp
+++ bowling/Game.hpp
@@ -1,10 +1,28 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
+struct Frame {
+    char firstRoll;
+    char secondRoll;
+    Frame(char first, char second) : firstRoll(first), secondRoll(second) {}
+};
+
+struct PlayerData {
+    std::string playerName;
+    std::vector<Frame> playerRolls;
+};
 
 class Game {
-    
-    public:
+    std::vector<PlayerData> players;
+    void fillPlayerName(int& currentCharIndex, std::string& playerName, const std::string& lineText);
+    void fillPlayerRolls(int& currentCharIndex,
+                         std::vector<Frame>& playerRolls,
+                         const std::string& lineText);
+
+   public:
     Game(){};
     void loadFromFile(const std::string& fileName);
+    const std::vector<PlayerData>& getPlayers() { return players; };
 };
