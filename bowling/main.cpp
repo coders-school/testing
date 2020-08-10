5c037342ca0dd137b47b5f630134c1b86d40111b
jakjan95 <jakubjanusz95@gmail.com>
2020-08-10 22:19:36 +0200

    initial version of input handler and manual for program

--- bowling/main.cpp
+++ bowling/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <cstring>
 
-int main(){
-    
+void printHelp() {
+    std::cout << "******   BOWLING GAME    ******\n";
+    std::cout << "\tDIRECTORY  \t-directory which contains input files for game\n";
+    std::cout << "\t[FILE]  \t-optional file to save results of game\n";
+    std::cout << "example of use:  ./bowling inputDirectory results.txt\n";
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 2 && strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+        printHelp();
+    }
+
+    std::string inputDirectoryName = argv[1];
+    std::string resultOutputFileName;
+
+    if(argc == 3){
+        resultOutputFileName = argv[2];
+    }
+
+    std::cout << inputDirectoryName << '\n';
+    if(!resultOutputFileName.empty()) {
+        std::cout << resultOutputFileName << '\n';
+    }
+
     return 0;
 }
