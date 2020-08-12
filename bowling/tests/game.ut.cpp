bfd40834abb95117bb9073c89c0e6aec4d7d1c0a
jakjan95 <jakubjanusz95@gmail.com>
2020-08-12 21:58:40 +0200

    Tests added to build

---
+++ bowling/tests/game.ut.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+
+// TEST(SortTestV4Fixture, GivenAVector3214WhenVectorIsSortedWithReverseIteratorsThenItIsEqualTo4321) {
+//     // GIVEN
+//     std::vector<int> v {3, 2, 1, 4};
+//     std::vector<int> expected {4, 3, 2, 1};
+
+//     // WHEN
+//     std::sort(v.rbegin(), v.rend());
+
+//     // THEN
+//     ASSERT_EQ(v, expected);
+// }
+
+TEST(FirstTest, ShouldBeTrue){
+    ASSERT_EQ(5, 5);
+}
