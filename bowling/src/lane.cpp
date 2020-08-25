a8ebdd3507af7bf41a6c3c4971d4bd3bf7f22347
damixiorek <damixiorek@gmail.com>
2020-08-25 15:09:31 +0200

    lane constructor and parseLaneName() done

--- bowling/src/lane.cpp
+++ bowling/src/lane.cpp
@@ -1,5 +1,40 @@
 #include "lane.hpp"
 
-Lane::Lane() {}
+#include <filesystem>
+
+// private
+
+void Lane::parseFile(std::string fileName)
+{
+    fileName = std::filesystem::path(fileName).stem();
+
+    if(fileName[0] == '.') {
+
+        std::string fileExtension = std::filesystem::path(fileName).extension();
+        std::size_t foundExtensionIndex = fileName.find(fileExtension);
+
+        if (foundExtensionIndex != std::string::npos) {
+            fileName.erase(foundExtensionIndex);
+        }
+
+    }
+
+    this->laneName_ = fileName;
+}
+
+void Lane::parseLaneName(std::string laneName) {}
+
+// public
+
+Lane::Lane(std::string fileName)
+{
+    this->parseFile(fileName);
+    this->parseLaneName(fileName);
+}
 
 Lane::~Lane() {}
+
+std::string Lane::getLaneName()
+{
+    return this->laneName_;
+}
