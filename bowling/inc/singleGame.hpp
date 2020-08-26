7c30d48a6addaf332a2901e3f215266ee18f44c5
damixiorek <damixiorek@gmail.com>
2020-08-26 16:50:55 +0200

    blueprint of SingleGame class

--- bowling/inc/singleGame.hpp
+++ bowling/inc/singleGame.hpp
@@ -1,8 +1,36 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 class SingleGame {
 private:
+    std::string gameInput_{};
+    std::string playerName_{};
+    std::vector<size_t> rolls_{};
+    size_t score_{};
+    
+    enum class GameStatus {
+        FINISHED,
+        IN_PROGRESS,
+        NOT_STARTED
+    };
+
+    GameStatus gameStatus_{};
+
+    void parseGameInput();
+    void readPlayerName();
+    void putScoresToVector();
+    void checkGameStatus();
+    void countScore();
+
 public:
-    SingleGame();
+    SingleGame(std::string& gameInput);
     ~SingleGame();
+
+    std::string getPlayerName() const;
+    size_t getScore() const;
+    enum GameStatus getGameStatus() const;
+    friend std::ostream& operator<<(std::ostream& os, const SingleGame& singleGame);
 };
--- bowling/src/singleGame.cpp
+++ bowling/src/singleGame.cpp
@@ -1,5 +1,38 @@
 #include "singleGame.hpp"
 
-SingleGame::SingleGame() {}
+SingleGame::SingleGame(std::string& gameInput)
+{
+    this->gameInput_ = gameInput;
+    parseGameInput();
+}
 
 SingleGame::~SingleGame() {}
+
+void SingleGame::parseGameInput() {
+    this->readPlayerName();
+    this->putScoresToVector();
+    this->checkGameStatus();
+}
+
+void SingleGame::readPlayerName() {}
+
+void SingleGame::putScoresToVector() {}
+
+void SingleGame::checkGameStatus() {}
+
+void SingleGame::countScore() {}
+
+std::string SingleGame::getPlayerName() const
+{
+    return this->playerName_;
+}
+
+size_t SingleGame::getScore() const
+{
+    return this->score_;
+}
+
+enum SingleGame::GameStatus SingleGame::getGameStatus() const
+{
+    return gameStatus_;
+}
