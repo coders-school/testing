f5e4f22851d7b688ba095dbfc56d3ffca11bb510
NG90 <glowka.natalia@gmail.com>
2020-08-31 23:17:51 +0200

    BowlingGame - tests

--- bowling/tests/bowlingGame.ut.cpp
+++ bowling/tests/bowlingGame.ut.cpp
@@ -2,7 +2,63 @@
 
 #include "bowlingGame.hpp"
 
-TEST_CASE()
+#include <sstream>
+
+using Catch::Matchers::StartsWith;
+
+SCENARIO("BowlingGame class can check if help is to be printed and trigger printing", "bowlingGame")
 {
-    REQUIRE(1 == 1);
+    int testArgc = 2;
+    std::string expectedHelpBeginning{"BowlingGame <input directory> <output file>"};
+
+    {
+        const char* testArgv[] = {"programName", "--help", NULL};
+
+        GIVEN("argc = " << testArgc
+                        << " and argv = {\""
+                        << (testArgv[0] ? testArgv[0] : "") << "\", \""
+                        << (testArgv[1] ? testArgv[1] : "") << "\", \""
+                        << (testArgv[2] ? testArgv[2] : "") << "\"}")
+        {
+            WHEN("BowlingGame object is created")
+            {
+                BowlingGame testBowlingGame(testArgc, testArgv);
+                THEN("isHelpToBePrinted() reeturns true and help is printed")
+                {
+                    REQUIRE(testBowlingGame.isHelpToBePrinted());
+
+                    std::stringstream ss;
+                    ss << testBowlingGame.printHelp();
+                    REQUIRE_FALSE(ss.str().empty());
+                    REQUIRE_THAT(ss.str(),
+                                 StartsWith(expectedHelpBeginning));
+                }
+            }
+        }
+    }
+    {
+        const char* testArgv[] = {"programName", "-h", NULL};
+
+        GIVEN("argc = " << testArgc
+                        << " and argv = {\""
+                        << (testArgv[0] ? testArgv[0] : "") << "\", \""
+                        << (testArgv[1] ? testArgv[1] : "") << "\", \""
+                        << (testArgv[2] ? testArgv[2] : "") << "\"}")
+        {
+            WHEN("BowlingGame object is created")
+            {
+                BowlingGame testBowlingGame(testArgc, testArgv);
+                THEN("isHelpToBePrinted() reeturns true and help is printed")
+                {
+                    REQUIRE(testBowlingGame.isHelpToBePrinted());
+
+                    std::stringstream ss;
+                    ss << testBowlingGame.printHelp();
+                    REQUIRE_FALSE(ss.str().empty());
+                    REQUIRE_THAT(ss.str(),
+                                 StartsWith(expectedHelpBeginning));
+                }
+            }
+        }
+    }
 }
