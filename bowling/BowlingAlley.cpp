36b3190def1ef07d693b6ed9576bf5b79e133733
Marcinati <marcin.kapusta.pk@gmail.com>
2020-09-11 21:46:37 +0200

    showing current path

--- bowling/BowlingAlley.cpp
+++ bowling/BowlingAlley.cpp
@@ -1,5 +1,10 @@
 #include "BowlingAlley.hpp"
+#include <iostream>
 
 BowlingAlley::BowlingAlley(std::filesystem::path path)
     : path_(path) 
     {}
+
+void BowlingAlley::showCurrentPath(std::filesystem::path path) {
+    std::cout << "Your current path is: " << std::filesystem::current_path() << 'n';
+}
--- bowling/BowlingAlley.hpp
+++ bowling/BowlingAlley.hpp
@@ -6,6 +6,7 @@
 class BowlingAlley : public Game {
 public:
     BowlingAlley(std::filesystem::path path);
+    void showCurrentPath(std::filesystem::path path);
 
 private:
     std::filesystem::path path_;
