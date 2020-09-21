df30648d6d14757df37e492ab3ec1d5772b1a31e
jakjan95 <jakubjanusz95@gmail.com>
2020-09-21 14:13:24 +0200

    Initial version of BowlingLane class

--- bowling/include/BowlingLane.hpp
+++ bowling/include/BowlingLane.hpp
@@ -1,5 +1,21 @@
 #pragma once
 
-class BowlingLine{
+#include <iostream>
+#include <vector>
 
+class Player;
+
+class BowlingLine {
+public:
+
+    void printData() {
+        std::cout << laneNumber_ << '\n';
+        for (const auto& el : players_) {
+            //std::cout << el << '\n';
+        }
+    }
+
+private:
+    int laneNumber_;
+    std::vector<Player> players_;
 };
