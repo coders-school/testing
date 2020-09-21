158f5facafcbf9bd69a3a7c51c7a928d1ca77310
jakjan95 <jakubjanusz95@gmail.com>
2020-09-21 14:10:28 +0200

    Player class Initial version

--- bowling/include/Player.hpp
+++ bowling/include/Player.hpp
@@ -1,4 +1,23 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 class Player {
+public:
+
+    Player(const std::string& name, const std::vector<int>& points)
+        : name_{name}, points_{points} {}
+
+    std::string getName() const {
+        return name_;
+    }
+
+    int getScore() {
+        return 0;
+    }
+
+private:
+    std::string name_;
+    std::vector<int> points_;
 };
