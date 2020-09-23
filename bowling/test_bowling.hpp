99835dd96442a90a4a46125c711e722770fa2a20
duszek <marcin.dus@gmail.com>
2020-09-24 00:15:54 +0200

    rest of scenarios - typos to review

--- bowling/test_bowling.hpp
+++ bowling/test_bowling.hpp
@@ -1,163 +1,98 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
-scenariusze testowe w formacie GIVEN WHEN THEN do programu, 
-którego zadaniem jest liczyć punkty w kręglach. Scenariusze mają być napisane we frameworku Catch2, 
-ale nie muszą zawierać żadnego kodu. Nawet nie musicie implementować tego programu (jeszcze 😉). 
-Chodzi o to, aby pokryć jak największą funkcjonalność testami. Opis zasad gry w kręgle poniżej.
-  * 1 punkt za każdy scenariusz testowy. Scenariusze pokrywające tę samą funkcjonalność będą liczone jako jeden.
-
-
-//catch example:
-SCENARIO("Conversion to Roman and to Arabic", "[toRoman][fromRoman]") {
-    GIVEN("All numbers from range 1 to 3999") {
-        auto number = GENERATE(range(1, 3999));
-
-        WHEN("toRoman and fromRoman are called for " << number) {
-            auto result = fromRoman(toRoman(number));
-
-            THEN("result is equal to number") {
-                CHECK(result == number);
-            }
-        }
-    }
+// sunny day scenarios
+//
+GIVEN("No ball played in first frame") {
+  WHEN("first ball in a frame knocks down 5 pins") {
+    THEN("The frame is not over. Another ball can be played ") {}
+  }
 }
 
+GIVEN("First ball played and knocks down 0 pins") {
+  WHEN("Second ball played and knocks down 10 pins") {
+    THEN("The frame is over. First frame includes spare") {}
+  }
+}
 
+GIVEN("First ball played and knocks down 1 pins") {
+  WHEN("Second ball played and knocks down 1 pins") {
+    THEN("The frame is over. ") { CHECK("Score for frame is 2") }
+  }
+}
 
+GIVEN("First ball played and knocks down 0 pins") {
+  WHEN("Second ball played and knocks down 0 pins") {
+    THEN("The frame is over. ") { CHECK("Score for frame is 0") }
+  }
+}
 
+GIVEN("First ball knocks down 0, second ball knocks down 10 pins, first frame "
+      "is over") {
+  WHEN("Second frame played with 9 score") {
+    THEN("First frame is a 'spare'.") { CHECK("score for first frame is 19") }
+  }
+}
 
-Each game, or "line" of bowling, includes ten turns,
-or "frames" for the bowler.
-
-GIVEN Player with 10 frames played and there is no strike in last frame
-WHEN  Player tries play ball and hits xxx points
-THEN  
-
-
-
-    GIVEN("Player with first ball played ") {
-        WHEN("Players play second ball AND knocks down 10 pins") {
-            THEN("First frame includes spare ") i
-            AND total result is equal to {
-            }
-        }
+GIVEN("First ball in first frame knocks down 10 pins, first fame is over.") {
+  WHEN("Second frame played with 9, third frame played with 9") {
+    THEN("First frame is over with 'strike'") {
+      CHECK("score for first frame is 28")
     }
+  }
+}
 
+// endigs
+GIVEN("9 frames are played. First ball in last frame is not played.") {
+  WHEN("First ball in 10th frameknocks down 10 pins") {
+    THEN(
+        "Last frame is over. Two bonus balls are added, game is not finished") {
 
-//In each frame, the bowler gets up to two tries to
-//knock down all ten pins.
+    }
+  }
+}
 
+GIVEN("All 10 frames are played, First ball in 10th frame knocks down 10 pins, "
+      "Last frame is over. Two bonus balls are added, game is not finished") {
+  WHEN(
+      "New pins are setup, first extra ball is played and knock down 10 pins") {
+    THEN("Game is over. Second bonus ball is not played. No additional bonus "
+         "balls") {}
+  }
+}
+}
 
+GIVEN("All 10 frames are played, First ball in 10th frame knocks down 10 pins, "
+      "Last frame is over. Two bonus balls are added, game is not finished") {
+  WHEN("First extra ball is played and knock down 3 pins") {
+    THEN("Game is not over. Second bonus ball can be played. No additional "
+         "bonus balls") {}
+  }
+}
 
-- player proboje grac 3 raz 
-- 
+GIVEN("First extra ball is played and knock down 3 pins") {
+  WHEN("Second extra ball is played and knock down 1 pin") {
+    THEN("Game is over. No extra balls added")
+  }
+}
 
-//If the first ball in a frame knocks down all ten pins,
-Given zadna pilka nie byla zagrana gra sie rozpoczela
+GIVEN("First extra ball is played and knock down 3 pins") {
+  WHEN("Second extra ball is played and knock down 7 pins") {
+    THEN("Game is over. No extra balls added")
+  }
+}
 
+// interruption scenarios
+SCENARIO("Playig agains the rules") {
+  GIVEN("Player with 2 balls played in 10-th frame and hits 3 points in last "
+        "frame") {
+    WHEN("Player tries play ball and hits all points (strike)") {
+      THEN("Last frame counts for 3 points") {}
+    }
+  }
 
-- pierwsz pilka straca wszystkie
-- pierwsza pilka nie srtaca zadnego
-- pierwszap ilka straca polowe
-
-
-
-this is called a "strike". The frame is over. 
-
-
-The score
-for the frame is ten plus the total of the pins knocked
-down in the next two balls.
-
-Given pierwsza pilka zagrana i stracila xxxx
-Given pierwsza pilka zagrana i stracila wszystkie
-Given pierwsza pilka zagrana i stracila nie stracila zadnego
-
-
-When druga pilka straca wszystkie 
-TEN mamy spare
-
-
-
-//If the second ball in a frame knocks down all ten pins,
-//this is called a "spare". The frame is over. The score
-//for the frame is ten plus the number of pins knocked
-//down in the next ball.
-
-
-
-
-//If, after both balls, there is still at least one of the
-
-Given 2 pilki zagrane 
- 1) nic nie stracone
- 2) troche stracone ale w drugim rzucie
- 3) troche stracone ale tylko w pierwszym rzucie
-
-
-
-Given  Player played 10 frames wit all mises 
-
-
-
-//ten pins standing the score for that frame is simply
-//the total number of pins knocked down in those two balls.
-//
-//
-//
-//
-//
-//If you get a spare in the last (10th) frame you get one
-//more bonus ball. If you get a strike in the last (10th)
-//frame you get two more bonus balls.
-//
-//
-//
-
-//These bonus throws are taken as part of the same turn.
-//If a bonus ball knocks down all the pins, the process
-//does not repeat. The bonus balls are only used to
-//calculate the score of the final frame.
-//
-//
-//
-
-//
-//The game score is the total of all frame scores.
-//
-//Examples:
-//
-//X indicates a strike
-/// indicates a spare
-//- indicates a miss
-//| indicates a frame boundary
-//The characters after the || indicate bonus balls
-//
-//X|X|X|X|X|X|X|X|X|X||XX
-//Ten strikes on the first ball of all ten frames.
-//Two bonus balls, both strikes.
-//Score for each frame == 10 + score for next two
-//balls == 10 + 10 + 10 == 30
-//Total score == 10 frames x 30 == 300
-//
-//9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||
-//Nine pins hit on the first ball of all ten frames.
-//Second ball of each frame misses last remaining pin.
-//No bonus balls.
-//Score for each frame == 9
-//Total score == 10 frames x 9 == 90
-//
-//5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5
-//Five pins on the first ball of all ten frames.
-//Second ball of each frame hits all five remaining
-//pins, a spare.
-//One bonus ball, hits five pins.
-//Score for each frame == 10 + score for next one
-//ball == 10 + 5 == 15
-//Total score == 10 frames x 15 == 150
-//
-//X|7/|9-|X|-8|8/|-6|X|X|X||81
-//Total score == 167
-//*/
-
+  GIVEN("Player1 played two balls in first frame and now Player2 should play") {
+    WHEN("Player1 tries to play ball") {
+      THEN("Not possible to play ball in this frame for Player1 ") {}
+    }
+  }
+}
