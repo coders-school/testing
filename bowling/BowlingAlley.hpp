16304366fe5b128fb719ef8394117ac212e1c228
WojtekMs <wojtek.atmo@gmail.com>
2020-10-12 13:44:07 +0200

    game aggregation added + default ctor

--- bowling/BowlingAlley.hpp
+++ bowling/BowlingAlley.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
 #include <filesystem>
+#include <vector>
 
 namespace fs = std::filesystem;
-
+class Game;
 class BowlingAlley {
 public:
+    BowlingAlley() = default;
     explicit BowlingAlley(const fs::path& pathToFolder);
+
     void showCurrentStatus();
 
 private:
     size_t countFiles();
-
-    fs::path path_;
+    std::vector<Game*> games_{};
+    fs::path path_{};
 };
