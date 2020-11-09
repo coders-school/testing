e64b190d81b8599fafc1e2c2e495cd5bf960a74c
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-11-09 08:44:41 +0100

    :recycle: default destructors aren't necessary

--- bowling/BowlingAlley.hpp
+++ bowling/BowlingAlley.hpp
@@ -8,7 +8,7 @@ namespace fs = std::filesystem;
 class Game;
 class BowlingAlley {
 public:
-    BowlingAlley() = default;
+    BowlingAlley() = delete;
     explicit BowlingAlley(const fs::path& pathToFolder);
     void loadFromFolder(const fs::path& pathToFolder);
     void printOutputTo(std::ostream&);
