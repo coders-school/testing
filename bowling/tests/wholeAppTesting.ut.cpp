3bd4aa34f0c1458684eee4e40061cc1222d5d92b
damixiorek <damixiorek@gmail.com>
2020-11-10 09:22:46 +0100

    add whole app tests, modified CMakeLists

---
+++ bowling/tests/wholeAppTesting.ut.cpp
@@ -0,0 +1,24 @@
+#include "catch.hpp"
+
+#include "argumentParser.hpp"
+#include "bowlingGame.hpp"
+#include "directoryHandler.hpp"
+#include "fileHandler.hpp"
+#include "lane.hpp"
+#include "singleGame.hpp"
+
+#include <utility>
+#include <vector>
+
+SCENARIO("")
+{
+    GIVEN("")
+    {
+        WHEN("")
+        {
+            THEN("")
+            {
+            }
+        }
+    }
+}
