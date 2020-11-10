8dc28ab0bccd0e86849aabb4de51c20164d15a33
Pawellos <pablox377@gmail.com>
2020-11-10 23:33:30 +0100

    BowlingLane tests for messages

--- bowling/tests/BowlingLane.ut.cpp
+++ bowling/tests/BowlingLane.ut.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
+#include <string_view>
 #include "../include/BowlingLane.hpp"
 
 constexpr char playerName[] = "test";
 constexpr char noGameString[] = "no game";
+constexpr char gameInProgressMessage[] = "### Lane 1: game in progress ###\ntest 3\n";
+constexpr char gameFinishedMessage[] = "### Lane 1: game finished ###\ntest 20\n";
 
 struct BowlingLaneTest : public ::testing::Test {
     std::vector<std::shared_ptr<Player>> players;
@@ -12,16 +15,6 @@ struct BowlingLaneTest : public ::testing::Test {
 TEST_F(BowlingLaneTest, ShouldConvertGameStateNoGameoString) {
     auto gameState = bowlingLane_.convertGameState(BowlingLane::GameState::NO_GAME);
     ASSERT_EQ(gameState, noGameString);
-}
-
-TEST_F(BowlingLaneTest, ShouldWriteOutDataUsingOstreamOperator) {
-    std::stringstream os;
-    os << bowlingLane_;
-    int laneNumber = bowlingLane_.getLaneNumber();
-    auto gameState = bowlingLane_.convertGameState(bowlingLane_.checkGameState());
-
-    std::string expected("### Lane " + std::to_string(laneNumber) + ": " + gameState + " ###\n");
-    ASSERT_EQ(os.str(), expected);
 }
 
 TEST(BowlingLane, BowlingLaneStateShouldReturnNoGame) {
@@ -40,8 +33,42 @@ TEST(BowlingLane, BowlingLaneStateShouldReturnGameInProgress) {
 
 TEST(BowlingLane, BowlingLaneStateShouldReturnFinished) {
     std::vector<std::shared_ptr<Player>> players;
-    std::vector<int> gameInProgress(20, 1);
-    players.emplace_back(std::make_shared<Player>(playerName, gameInProgress));
+    std::vector<int> gameFinished(20, 1);
+    players.emplace_back(std::make_shared<Player>(playerName, gameFinished));
     BowlingLane bowlingLane{1, players};
     ASSERT_EQ(bowlingLane.checkGameState(), BowlingLane::GameState::FINISHED);
+}
+
+TEST_F(BowlingLaneTest, ShouldWriteOutDataUsingOstreamOperatorWithEmptyLaneNoGame) {
+    std::stringstream os;
+    os << bowlingLane_;
+    int laneNumber = bowlingLane_.getLaneNumber();
+    auto gameState = bowlingLane_.convertGameState(bowlingLane_.checkGameState());
+
+    std::string expected("### Lane " + std::to_string(laneNumber) + ": " + gameState + " ###\n");
+    ASSERT_EQ(os.str(), expected);
+}
+
+TEST_F(BowlingLaneTest, ShouldWriteOutDataUsingOstreamOperatorWithGameInProgress) {
+    std::vector<std::shared_ptr<Player>> players;
+    std::vector<int> gameInProgress = {1, 2, 3};
+    players.emplace_back(std::make_shared<Player>(playerName, gameInProgress));
+    BowlingLane bowlingLane_{1, players};
+
+    std::stringstream os;
+    os << bowlingLane_;
+
+    ASSERT_EQ(os.str(), gameInProgressMessage);
+}
+
+TEST_F(BowlingLaneTest, ShouldWriteOutDataUsingOstreamOperatorWithGameFinished) {
+    std::vector<std::shared_ptr<Player>> players;
+    std::vector<int> gameFinished(20, 1);
+    players.emplace_back(std::make_shared<Player>(playerName, gameFinished));
+    BowlingLane bowlingLane_{1, players};
+
+    std::stringstream os;
+    os << bowlingLane_;
+
+    ASSERT_EQ(os.str(), gameFinishedMessage);
 }
