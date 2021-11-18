46f5bf068258fb2249f12acf023e0215960823d2
miczel <michal.soloducha@gmail.com>
2021-11-18 12:30:36 +0100

    Add result codes handling

--- test.cpp
+++ test.cpp
@@ -1,73 +1,91 @@
 #include <string>
 #include "catch.hpp"
 
+enum class ResultCode {
+    OK,
+    BadCharacter,
+    BadFormat,
+    UndefinedError
+};
+
 // dummy function
-unsigned short calculateTotalScore(std::string) {
-    return 301;
+ResultCode calculateTotalScore(const std::string& frameScores, int* totalScore) {
+    *totalScore = -1;  // result impossible to achieve
+    return ResultCode::UndefinedError;
 }
 
 SCENARIO("The calculateTotalScore() function calculates a total bowling score for given frame scores", "[bowling][totalScore]") {
     GIVEN("the frame scores: \"X|X|X|X|X|X|X|X|X|X||XX\"") {
         std::string frameScores{"X|X|X|X|X|X|X|X|X|X||XX"};
+        int totalScore{-1};
 
         WHEN("total score is calculated") {
-            auto totalScore = calculateTotalScore(frameScores);
+            ResultCode resultCode = calculateTotalScore(frameScores, &totalScore);
 
             THEN("the expected total score is") {
-                unsigned short expectedTotalScore{300};
+                int expectedTotalScore{300};
                 REQUIRE(totalScore == expectedTotalScore);
+                REQUIRE(resultCode == ResultCode::OK);
             }
         }
     }
 
     GIVEN("the frame scores: \"9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||\"") {
         std::string frameScores{"9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||"};
+        int totalScore{-1};
 
         WHEN("total score is calculated") {
-            auto totalScore = calculateTotalScore(frameScores);
+            ResultCode resultCode = calculateTotalScore(frameScores, &totalScore);
 
             THEN("the expected total score is") {
-                unsigned short expectedTotalScore{90};
+                int expectedTotalScore{90};
                 REQUIRE(totalScore == expectedTotalScore);
+                REQUIRE(resultCode == ResultCode::OK);
             }
         }
     }
 
     GIVEN("the frame scores: \"5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5\"") {
         std::string frameScores{"5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5"};
+        int totalScore{-1};
 
         WHEN("total score is calculated") {
-            auto totalScore = calculateTotalScore(frameScores);
+            ResultCode resultCode = calculateTotalScore(frameScores, &totalScore);
 
             THEN("the expected total score is") {
-                unsigned short expectedTotalScore{150};
+                int expectedTotalScore{150};
                 REQUIRE(totalScore == expectedTotalScore);
+                REQUIRE(resultCode == ResultCode::OK);
             }
         }
     }
-    
+
     GIVEN("the frame scores: \"X|7/|9-|X|-8|8/|-6|X|X|X||81\"") {
         std::string frameScores{"X|7/|9-|X|-8|8/|-6|X|X|X||81"};
+        int totalScore{-1};
 
         WHEN("total score is calculated") {
-            auto totalScore = calculateTotalScore(frameScores);
+            ResultCode resultCode = calculateTotalScore(frameScores, &totalScore);
 
             THEN("the expected total score is") {
-                unsigned short expectedTotalScore{167};
+                int expectedTotalScore{167};
                 REQUIRE(totalScore == expectedTotalScore);
+                REQUIRE(resultCode == ResultCode::OK);
             }
         }
     }
 
     GIVEN("the frame scores: \"-|-|-|-|-|-|-|-|-|-||--\"") {
         std::string frameScores{"-|-|-|-|-|-|-|-|-|-||--"};
+        int totalScore{-1};
 
         WHEN("total score is calculated") {
-            auto totalScore = calculateTotalScore(frameScores);
+            ResultCode resultCode = calculateTotalScore(frameScores, &totalScore);
 
             THEN("the expected total score is") {
-                unsigned short expectedTotalScore{0};
+                int expectedTotalScore{0};
                 REQUIRE(totalScore == expectedTotalScore);
+                REQUIRE(resultCode == ResultCode::OK);
             }
         }
     }
